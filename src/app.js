95194c136c6fe5cd2554af9dbc5e4847a36c9561
Cléber Severo <clebersevero1998@gmail.com>
2023-10-14 23:01:18 -0300

    doc: server connection string exported to .env variable

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import conectaNaDataBase from './config/dbConnect.js';
+import livro from './models/Livro.js';
 
 const conexao = await conectaNaDataBase();
 
@@ -13,31 +14,15 @@ conexao.once("open", () => {
 })
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-const livros = [
-    {
-        id: 1,
-        titulo: "O Senhor dos Aneis"
-    },
-    {
-        id: 2,
-        titulo: "O Hobbit"
-    }
-]
-
-function buscaLivro(id) {
-    return livros.findIndex(livro => {
-        return livro.id === Number(id);
-    });
-}
-
-app.get("/", (req, res) => {
+app.get("/",  (req, res) => {
     res.status(200).send("Curso de Node.js");
 });
 
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
+app.get("/livros", async (req, res) => {
+    const listaLivros = await livro.find({})
+    res.status(200).json(listaLivros);
 });
 
 app.get("/livros/:id", (req,res) => {
--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose'
 
 async function conectaNaDataBase() {
-    mongoose.connect("mongodb+srv://admin:admin123@cluster0.fc6jwge.mongodb.net/livraria?retryWrites=true&w=majority");
-
+    mongoose.connect(process.env.DB_CONNECTION_STRING);
     return mongoose.connection;
 } 
 
