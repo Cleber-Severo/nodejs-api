19b572003293cf19b45ab86fb9e9b8de2cb6441b
Cléber Severo <clebersevero1998@gmail.com>
2023-10-19 22:26:53 -0300

    feat: livro put controller

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,6 +4,9 @@ import LivroController from '../Controllers/LivroController.js'
 const routes = express.Router();
 
 routes.get("/livros", LivroController.listarLivros);
+routes.get("/livros/:id", LivroController.listarLivroPorId);
+routes.post("/livros", LivroController.cadastrarLivro);
+routes.put("/livros/:id", LivroController.atualizarLivro);
 
 
 export default routes
